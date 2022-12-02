e72fd6404febd8618b2fd8fbf3c5b87523c5c409
Kyle Vanderstoep <kyle@contou.com>
2022-12-02 13:12:49 -0700

    Failure Modes changed to list view

--- CMMS/Graph/Maintenance/WOFailureModeMaint.cs
+++ CMMS/Graph/Maintenance/WOFailureModeMaint.cs
@@ -3,8 +3,10 @@ using PX.Data.BQL.Fluent;
 
 namespace CMMSlite.WO
 {
-    public class WOFailureModeMaint : PXGraph<WOFailureModeMaint, WOFailureMode>
+    public class WOFailureModeMaint : PXGraph<WOFailureModeMaint>
     {
+        public PXCancel<WOFailureMode> Cancel;
+        public PXSave<WOFailureMode>   Save;
         [PXViewName(Messages.ViewFailureModes)]
         public SelectFrom<WOFailureMode>.View FailureModes;
     }
