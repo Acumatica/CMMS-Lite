980795c0698af4837d6918461ee1d7d0efa09845
Brian Stevens <brian.stevens@setech.com>
2023-10-24 16:44:10 -0500

    Add WO201000 and WO301000 base TypeScript files

---
+++ CMMS_23_106_0050/FrontendSources/screen/src/screens/WO/WO301000/WO301000.ts
@@ -0,0 +1,35 @@
+﻿import {
+	ScreenBaseViewModel,
+	ActionState,
+	createInstance,
+	createCollection,
+	graphInfo,
+	localizable
+} from 'client-controls';
+
+import {
+	WOOrder,
+	WOLine,
+	//WOLineItem,
+	//WOLineLabor,
+	//WOLineTool,
+	//WOLineMeasure,
+	//WOLineFailure,
+	WOSchedule
+} from './views';
+
+@localizable
+class TabHeaders {
+	static Details = "Details";
+}
+
+@graphInfo({ graphType: 'CMMS.WOOrderEntry', primaryView: 'Document' })
+export class WO301000 extends ScreenBaseViewModel {
+	TabHeaders = TabHeaders;
+
+	Document = createInstance(WOOrder);
+	CurrentDocument = createInstance(WOOrder);
+
+
+}
+
