715d00e860e2ca771904eb44b2cb9850c1f2ea0c
Brian Stevens <brian.stevens@setech.com>
2023-10-27 10:42:46 -0500

    WO301000 - Modern UI - Incremental Progress - Still need to add more columns to tabs, but all tabs, including child tabs, are present now.  Needs a lot of cleanup before it can be used.

--- CMMS_23_106_0050/FrontendSources/screen/src/screens/WO/WO301000/WO301000.ts
+++ CMMS_23_106_0050/FrontendSources/screen/src/screens/WO/WO301000/WO301000.ts
@@ -10,17 +10,28 @@
 import {
 	WOOrder,
 	WOLine,
-	//WOLineItem,
-	//WOLineLabor,
-	//WOLineTool,
-	//WOLineMeasure,
-	//WOLineFailure,
-	WOSchedule
+	WOLineItem,
+	WOLineLabor,
+	WOLineTool,
+	WOLineMeasure,
+	WOLineFailure,
+	CSAnswer,
+	WOApproval,
+	WOOrder4
 } from './views';
 
 @localizable
 class TabHeaders {
 	static Details = "Details";
+	static Operations = "Operations";
+	static Labor = "Labor";
+	static Materials = "Materials";
+	static Tools = "Tools";
+	static Measurements = "Measurements";
+	static FailureModes = "Failure Modes";
+	static Attributes = "Attributes";
+	static Approvals = "Approvals";
+	static RelatedWO = "Related Work Orders";
 }
 
 @graphInfo({ graphType: 'CMMS.WOOrderEntry', primaryView: 'Document' })
@@ -30,6 +41,56 @@ export class WO301000 extends ScreenBaseViewModel {
 	Document = createInstance(WOOrder);
 	CurrentDocument = createInstance(WOOrder);
 
+	Transactions = createCollection(WOLine, {
+		initNewRow: true,
+		syncPosition: true
+	});
+
+
+
+	LineItems = createCollection(WOLineItem, {
+		initNewRow: true,
+		syncPosition: true
+	});
+
+	LineLabor = createCollection(WOLineLabor, {
+		initNewRow: true,
+		syncPosition: true
+	});
+
+	LineTools = createCollection(WOLineTool, {
+		initNewRow: true,
+		syncPosition: true
+	});
+
+	LineMeasurements = createCollection(WOLineMeasure, {
+		initNewRow: true,
+		syncPosition: true
+	});
+
+	LineFailureModes = createCollection(WOLineFailure, {
+		initNewRow: true,
+		syncPosition: true
+	});
+
+
+
+	Answers = createCollection(CSAnswer);
+
+	Approval = createCollection(
+		WOApproval,
+		{
+			allowInsert: false,
+			allowUpdate: false,
+			allowDelete: false,
+			columnsSettings: [
+				{ field: "Status", allowUpdate: false },
+				{ field: "Reason", allowUpdate: false },
+			],
+		}
+	);
+
+	RelatedWO = createCollection(WOOrder4);
 
 }
 
